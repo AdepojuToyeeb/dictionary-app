b15c4bb2f2b88fba134509928b153cb6a2e43bec
Adepoju Adewale <adewale.adepoju14@gmail.com>
2022-04-27 07:55:23 +0100

    fetched data from api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
 
 function App() {
+  const [meaning, setMeaning] = useState([]);
+
+  const dict = async () => {
+    try {
+      const data = await axios.get('https://api.dictionaryapi.dev/api/v2/entries/en/plane');
+      // const data = await response.json();
+      // console.log(data);
+      setMeaning(data.data)
+      
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  console.log(meaning)
+  useEffect (() => {
+    dict();
+  }, []);
+
   return (
     <div>App</div>
   )
